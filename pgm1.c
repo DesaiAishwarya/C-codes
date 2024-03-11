adc1de71a99581cb4820102f96ea87fec7e96c4e
DesaiAishwarya <120625085+DesaiAishwarya@users.noreply.github.com>
2024-03-11 23:23:10 +0530

    Add files via upload

---
+++ pgm1.c
@@ -0,0 +1,12 @@
+//adding of two numbers
+#include<stdio.h>
+ int main()
+ {
+     int a,b,add;
+     printf("Enter two numbers\n");
+     scanf("%d%d",&a,&b);
+     add=a+b;
+     printf("%d\n",add);
+
+     return 0;
+ }
